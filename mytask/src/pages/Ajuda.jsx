f0029f29d77dffc965c111b1eb97405df3928d72
almir-soulcode <jose.almir@soulcode.com>
2024-07-10 14:09:10 -0300

    aula1007: bootstrap showcase

--- mytask/src/pages/Ajuda.jsx
+++ mytask/src/pages/Ajuda.jsx
@@ -1,9 +1,31 @@
+import { Container, Accordion, Button, Alert } from "react-bootstrap";
+
 function Ajuda() {
-    return (
-        <main>
-            <h1>Ajuda</h1>
-        </main>
-    );
+  return (
+    <main>
+      <Container>
+        <h1>Ajuda</h1>
+        <hr />
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>Sobre o nosso sistema</Accordion.Header>
+                <Accordion.Body>
+                    MyTask é um sistema para gerenciamento de tarefas pessoais. Nesta aplicação você pode gerenciar todos os seus afazeres.
+                </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+                <Accordion.Header>Sobre a empresa</Accordion.Header>
+                <Accordion.Body>
+                    Esta empresa desenvolve soluções focadas em produtividade e bem-estar.
+                </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+        <Button className="mt-1 me-1">Entre em contato</Button>
+        <Button className="mt-1 me-1" variant="outline-danger">Denunciar</Button>
+        <Alert className="mt-1" variant="warning">Atenção! Estamos em manutenção.</Alert>
+      </Container>
+    </main>
+  );
 }
 
 export default Ajuda;
