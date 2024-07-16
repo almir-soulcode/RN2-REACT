6ce4921e79ade88c68ee5d693bf2596d974b9c65
almir-soulcode <jose.almir@soulcode.com>
2024-07-16 17:19:52 -0300

    aula1607: cadastro + google

---
+++ mytask/src/firebase/auth.js
@@ -0,0 +1,23 @@
+// O objetivo deste arquivo é ter
+// todas as funções relacionadas a autenticação
+// criar usuario com email/senha/nome
+// entrar com google
+// entrar com email/senha
+// logout
+import { createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup, updateProfile } from "firebase/auth";
+import { auth } from "./config";
+
+export async function cadastrarUsuario(nome, email, senha) {
+  // Indicamos o serviço de autenticação e o email e senha do novo usuário
+  // 'user' é um objeto com informações do usuário autenticado
+  const { user } = await createUserWithEmailAndPassword(auth, email, senha);
+  // Define o nome de exibição como o nome vindo do formulário de cadastro
+  await updateProfile(user, { displayName: nome });
+}
+
+export async function entrarGoogle() {
+    // Indicar qual o provedor de login será usado
+    const provider = new GoogleAuthProvider();
+    // Abre um pop-up na tela com o login do google
+    await signInWithPopup(auth, provider);
+}
--- mytask/src/pages/Cadastro.jsx
+++ mytask/src/pages/Cadastro.jsx
@@ -1,12 +1,33 @@
 import { Button } from "react-bootstrap";
 import { useForm } from "react-hook-form";
+import { cadastrarUsuario, entrarGoogle } from "../firebase/auth";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 function Cadastro() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
 
   function cadastrar(data) {
-    console.log("Cadastro!");
-    console.log(data);
+    cadastrarUsuario(data.nome, data.email, data.senha)
+      .then(() => {
+        toast.success(`Bem-vindo(a)! ${data.nome}`);
+        navigate("/tarefas");
+      })
+      .catch((error) => {
+        toast.error("Um erro aconteceu: " + error.code);
+      });
+  }
+
+  function handleEntrarGoogle() {
+    entrarGoogle().then(() => {
+      toast.success("Bem-vindo(a)!");
+      navigate("/tarefas");
+    });
   }
 
   return (
@@ -16,38 +37,47 @@ function Cadastro() {
         <hr />
         <div>
           <label htmlFor="nome">Nome</label>
-          <input 
-            type="text" 
-            id="nome" 
-            className="form-control" 
-            {...register("nome", {required: true, maxLength: 150})}
-            />
-            {errors.nome && <small className="invalid">O nome é inválido!</small>}
+          <input
+            type="text"
+            id="nome"
+            className="form-control"
+            {...register("nome", { required: true, maxLength: 150 })}
+          />
+          {errors.nome && <small className="invalid">O nome é inválido!</small>}
         </div>
         <div>
           <label htmlFor="email">Email</label>
-          <input 
-            type="email" 
-            id="email" 
-            className="form-control" 
+          <input
+            type="email"
+            id="email"
+            className="form-control"
             {...register("email", { required: true })}
-            />
-            {errors.email && <small className="invalid">O email é inválido!</small>}
+          />
+          {errors.email && (
+            <small className="invalid">O email é inválido!</small>
+          )}
         </div>
         <div>
           <label htmlFor="senha">Senha</label>
-          <input 
-            type="password" 
-            id="senha" 
-            className="form-control" 
-            {...register("senha", {required: true, minLength: 6})}
-            />
-            {errors.senha && <small className="invalid">A senha é inválida!</small>}
+          <input
+            type="password"
+            id="senha"
+            className="form-control"
+            {...register("senha", { required: true, minLength: 6 })}
+          />
+          {errors.senha && (
+            <small className="invalid">A senha é inválida!</small>
+          )}
         </div>
         <Button variant="dark" className="mt-1 w-100" type="submit">
           Cadastrar
         </Button>
-        <Button variant="danger" className="mt-1 w-100" type="button">
+        <Button
+          onClick={handleEntrarGoogle}
+          variant="danger"
+          className="mt-1 w-100"
+          type="button"
+        >
           Entrar com o Google
         </Button>
       </form>
